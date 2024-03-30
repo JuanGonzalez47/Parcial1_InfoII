5d6ecd53024faf1b9e906117041235b9a9085a6d
Juan González <161348171+JuanGonzalez47@users.noreply.github.com>
2024-03-30 11:37:02 -0500

    Update Prototipos.h

--- Parcial1-Main/Prototipos.h
+++ Parcial1-Main/Prototipos.h
@@ -4,7 +4,6 @@
 #endif // PROTOTIPOS_H
 
 int*** generar_matrices(int n_matrix,int tam_inicial);
-int* dimensio_variables(int n_matrix,int tam_inicial);
 void rotations(int ***arrSup, int dim, int NumberMatrix, int state);
 void compareFunction(int ***ptrPrincipal, int cont1, int cont2, int *ptrCond, int WichCond, int Pos1, int Pos2, int dimensionMatrixToRotate);
 void cambiar_dimension_matriz(int ***arreglo,int *dimen,int numberMatrix,int tam_inicial);
